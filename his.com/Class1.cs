2526b1e235f1394faf71a2ee76e5f209ffcaf59a
likui <Administrator@ICOS-20180927OL>
2018-09-27 18:09:38 +0800

    创建

---
+++ his.com/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace his.com
+{
+    public class Class1
+    {
+    }
+}
